18cb9c588e17c2b16a22cf87203f3dc944b88d56
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-03-18 13:59:35 +0545

    add

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -1,1 +1,12 @@
-body = document.querySelector('body');
+body = document.querySelector("body");
+add = document.querySelector(".add");
+list = document.querySelector(".list");
+
+add.addEventListener("click", () => {
+  task = document.getElementById("task");
+  list.innerHTML += `
+  <div class="todo">
+          <li>${task.value}<i class="fa fa-trash-alt"></i><i class="fa fa-check"></i></li>
+  </div>
+  `;
+});
