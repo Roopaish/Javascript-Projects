40c457760600ac20b430256454f1cc009ac617f3
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-03-19 09:31:22 +0545

    localstorage

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -1,12 +1,92 @@
-body = document.querySelector("body");
-add = document.querySelector(".add");
-list = document.querySelector(".list");
+// DOM
+const addTodo = document.querySelector(".add");
+const Todolist = document.querySelector(".list");
+const deleteButton = document.querySelector(".delete");
+const completedButton = document.querySelector(".completed");
+const filterButton = document.getElementsByName("filter");
+const err = document.querySelector(".error");
+let todos;
 
-add.addEventListener("click", () => {
-  task = document.getElementById("task");
-  list.innerHTML += `
-  <div class="todo">
-          <li>${task.value}<i class="fa fa-trash-alt"></i><i class="fa fa-check"></i></li>
-  </div>
-  `;
+// Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
+addTodo.addEventListener("click", addTodos);
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTodos();
+  }
 });
+deleteButton.addEventListener("click", deleteTodos);
+completedButton.addEventListener("click", completedTodos);
+
+// Functions
+
+function getTodos() {
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    addHTML(todo);
+  });
+}
+
+function addTodos() {
+  const newTask = document.getElementById("task");
+  if (!checkRedundancy(newTask.value)) {
+    addHTML(newTask.value);
+    saveLocalTodos(newTask.value);
+  }
+}
+
+function checkRedundancy(todo) {
+  let i;
+  for (i = 0; i < todos.length; i++) {
+    if (todos[i].toLowerCase() == todo.toString().toLowerCase()) {
+      break;
+    }
+  }
+  if (i == todos.length) {
+    return false;
+  } else {
+    if (confirm("Duplicate task! Do you want to add it anyway?")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+function addHTML(task) {
+  Todolist.innerHTML += `
+  <div class="todo">
+    <li>
+      <span class="task-value">${task}</span>
+
+      <button class="completed">
+        <i class="fa fa-trash-alt fa-2x"></i>
+      </button>
+
+      <button class="delete">
+        <i class="fa fa-check fa-2x"></i>
+      </button>
+
+    </li>
+  </div>
+ `;
+  err.innerText = "";
+}
+
+function saveLocalTodos(todo) {
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function deleteTodos(e) {}
+
+function completedTodos(e) {}
