49d41f2f18c905325b79e5af4292b74460a3974c
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-05-08 11:38:12 +0545

    :runner: Player Created

--- Shooting Game/app.js
+++ Shooting Game/app.js
@@ -1,1 +1,27 @@
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player{
+  constructor(x,y,radius,color){
+    this.x = x 
+    this.y = y 
+    this.radius = radius 
+    this.color = color 
+  }
+
+  draw(){
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+const x = canvas.width / 2;
+const y = canvas.width / 2;
+
+const player =new Player(x,y,30,'white')
+player.draw()
