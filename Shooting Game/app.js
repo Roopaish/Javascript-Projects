888fe8d0314a6ba6e037886219d1354f45d6b4d1
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-05-08 12:36:45 +0545

    :red_circle:Projectile | Bullet

--- Shooting Game/app.js
+++ Shooting Game/app.js
@@ -1,9 +1,12 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+// 2D canvas Setup
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth
+canvas.height = innerHeight
 
+
+// Player class
 class Player{
   constructor(x,y,radius,color){
     this.x = x 
@@ -23,5 +26,52 @@ class Player{
 const x = canvas.width / 2;
 const y = canvas.width / 2;
 
-const player =new Player(x,y,30,'white')
-player.draw()
+const player = new Player(x,y,30,'white')
+
+
+// Projectiles to be fired by Player
+class Projectile extends Player{
+  constructor(x,y,radius,color,velocity){
+    super(x,y,radius,color)
+    this.velocity = velocity
+  }
+
+  update(){
+    this.x += this.velocity.xV
+    this.y += this.velocity.yV
+  }
+}
+
+const projectiles = [
+  new Projectile(x, y, 5, 'pink',{xV : 1, yV : 1})
+]
+
+
+addEventListener('click',(e)=>{
+  const Xposn = e.clientX || e.touches[0].clientX  
+  const Yposn = e.clientY || e.touches[0].clientY
+  const angle = Math.atan2(Yposn - x, Xposn - y)
+  const velocity = {
+    xV : Math.cos(angle),
+    yV : Math.sin(angle)
+  }
+
+  projectiles.push(
+    new Projectile(x, y, 5, 'red',velocity)
+  )
+
+})
+
+function animate(){
+  requestAnimationFrame(animate)
+  // Clearing canvas by applying big Rect on Top of canvas before firing another projectile
+  ctx.clearRect(0, 0, 2 * x, 2 * y) 
+  player.draw() 
+
+  projectiles.forEach((projectile)=>{
+    projectile.draw()
+    projectile.update()
+  })
+}
+
+animate()
